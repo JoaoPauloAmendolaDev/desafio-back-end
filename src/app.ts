ad99d670eea39b6b4ba1f83ada865bebd7d766ac
João Paulo Silva Amendola <joaoa@id.uff.br>
2023-04-04 17:14:07 -0300

    feat: project configuration done

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+import "reflect-metadata"
+import "express-async-errors"
+import express, {Express} from "express"
+import cors from "cors"
+import { connectDb, disconnectDB, loadEnv } from "./config"
+
+loadEnv()
+
+const app = express()
+
+app
+   .use(cors)
+   .get("/health", (_req, res) => res.send("OK!"))
+
+
+export function init(): Promise<Express> {
+    connectDb()
+    return Promise.resolve(app)
+}
+
+export async function close(): Promise<void> {
+    await disconnectDB()
+}
+
+export default app
